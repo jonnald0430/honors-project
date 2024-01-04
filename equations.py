ccccbfb61e14c51e0db3c465ec17040713ea1215
jonnald0430 <hernandez29@my.smccd.edu>
2024-01-04 00:36:45 -0800

    user input for equations

---
+++ equations.py
@@ -0,0 +1,95 @@
+'''
+notes:
+
+these are equations to be somehow combined with our GUI
+mass is in metres and takes user input
+make sure grav constant and speed of light are in METRES
+
+figure out how to do error handling for conversions()
+
+'''
+
+import numpy as np 
+import scipy.constants as scipy
+import numpy as np
+import matplotlib
+
+time_conversions_seconds = {
+
+    'hour': 3600,
+    'day': 86400,
+    'month': 2.628e+6,
+    'year': 3.154e+7,
+
+}
+
+G = scipy.gravitational_constant
+C = scipy.speed_of_light
+
+def schwarzchild_radius(gravitational_constant, mass ,speed_of_light):
+    #calculates the radius the users input needs to be in 
+    return (2* float(gravitational_constant) * float(mass) / float(speed_of_light**2))       
+        
+def conversion_seconds(seconds, time_int, time_conv):
+    #calculates conversions (in seconds)
+    return seconds * (time_int/time_conv)
+
+
+radius = []
+while True:
+    mass = input('Enter an integer for black hole mass: ')
+
+    if mass.isdigit():
+        mass = float(mass)
+        radius = np.append(radius, schwarzchild_radius(G, mass, C))
+        print(f'Your radius is {radius[0]}')
+        break
+    else:
+        print('Please enter a valid integer')
+
+conversion_val = []
+while True:
+    seconds = input('Choose any number of seconds: ')
+
+    if seconds.isdigit():
+        seconds = int(seconds)
+        break
+    else:
+        print('Please enter a valid integer')
+
+while True:
+    conversion = input('What calendrical unit would you like to convert into the amount of seconds you inputted? (hour, day, month, year): ')
+    if conversion.lower() == 'hour' or conversion.upper() == 'HOUR':
+        conversion_val = np.append(conversion_val, conversion_seconds(seconds, 60, time_conversions_seconds['hour']))
+        print(f'You would be at this position for: {conversion_val[0]} hour(s).')
+        break
+    elif conversion.lower() == 'day' or conversion.upper() == 'DAY':
+        conversion_val = np.append(conversion_val, conversion_seconds(seconds, 24, time_conversions_seconds['day']))
+        print(f'You would be at this position for: {conversion_val[0]} day(s).')
+        break
+    elif conversion.lower() == 'month' or conversion.upper() == 'MONTH':
+        conversion_val = np.append(conversion_val, conversion_seconds(seconds, 30, time_conversions_seconds['month']))
+        print(f'You would be at this position for: {conversion_val[0]} month(s).')
+        break
+    elif conversion.lower == 'year' or conversion.upper() == 'YEAR':
+        conversion_val = np.append(conversion_val, conversion_seconds(seconds, 365, time_conversions_seconds['year']))
+        print(f'You would be at this position for: {conversion_val[0]} year(s).')
+        break
+    else:
+        print('Invalid, try again')
+
+valid = True
+distance_limit = radius[0]
+while valid:    
+    black_hole_distance = int(input('Your distance from the black hole: '))
+    if black_hole_distance > distance_limit:
+        valid == True
+    else:
+        black_hole_distance = print(f'You are in range of the Schwarzchild radius which means you (including light) cannot escape, thus we would not know what would happen enter a value greater than {distance_limit} meters: ')
+
+
+
+
+
+
+
